01e6b12177c47af2d07379ab2365173935ab193a
crivano <renato.crivano@gmail.com>
2019-11-18 16:09:24 -0300

    Enviando o número 581 antes do código do tipo da petição intercorrente

--- src/main/resources/br/jus/trf2/apoloapi/usuario-username-processo-numero-peticao-intercorrente-tipos-documento-get.sql
+++ src/main/resources/br/jus/trf2/apoloapi/usuario-username-processo-numero-peticao-intercorrente-tipos-documento-get.sql
@@ -1,4 +1,4 @@
-select p.codtippet as id, p.descr nome from tipopeticao p 
+select concat('581',p.codtippet) as id, p.descr nome from tipopeticao p 
  where p.indativo='S' 
  and upper(p.descr) not like '%PETIÇÃO INICIAL%' 
 
